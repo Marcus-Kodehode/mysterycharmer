8fb2299899172f7373c36b3775bebd1c918877fc
Marcus <marcus29sfj@gmail.com>
2025-09-05 18:46:32 +0200

    Turbo save — vroom

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import { AppStateProvider } from "./components/core/AppState";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <AppStateProvider>
           <div className="grid h-full grid-rows-[auto_1fr_auto]">
             <Header />
+              <Analytics/>
             <main id="content" className="min-h-0 overflow-y-auto">{children}</main>
             <Footer />
           </div>
